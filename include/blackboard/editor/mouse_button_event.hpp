ef8adc392b00b42bc6113c5643657eafd2c327aa
Marvin Marjan <marvin.marjan@gmail.com>
2026-05-10 15:31:22 -0400

    add dragging and click events for `MouseButtonEvent`

--- include/blackboard/editor/mouse_button_event.hpp
+++ include/blackboard/editor/mouse_button_event.hpp
@@ -1,29 +1,55 @@
 #pragma once
 
+#include <optional>
+
 #include <blackboard/event.hpp>
 #include <blackboard/updateable.hpp>
+#include <blackboard/editor/mouse_position_provider.hpp>
 
 
 
 
 class MouseButtonEvent : public Updateable
 {
+private:
+    std::optional<Vector2> press_position_;
+
+    float min_drag_distance_ = 5;
+    bool is_drag_ = false;
+
+
 public:
     using EventType = Event<>;
 
 
+    const MousePositionProvider& mouse_position_provider;
+
     int button_id;
 
-    EventType on_press, on_release, on_down;
+    EventType on_click, on_press, on_release, on_down;
+    EventType on_drag_start, on_drag_end;
 
 
-    explicit MouseButtonEvent(const int button_id) noexcept : button_id(button_id) {}
+    explicit MouseButtonEvent(const int button_id, const MousePositionProvider& mouse_position_provider = {}) noexcept
+        : button_id(button_id), mouse_position_provider(mouse_position_provider) {}
 
 
     void update() noexcept override;
 
 
+    bool is_clicked() const noexcept { return on_click.triggered(); }
     bool is_pressed() const noexcept { return on_press.triggered(); }
     bool is_released() const noexcept { return on_release.triggered(); }
     bool is_down() const noexcept { return on_down.triggered(); }
+
+
+private:
+    void update_drag_state() noexcept;
+
+    void trigger_press_event() noexcept;
+    void trigger_release_event() noexcept;
+    void trigger_down_event() noexcept;
+
+
+    float distance_from_press_position() const noexcept;
 };
--- src/editor/mouse_button_event.cpp
+++ src/editor/mouse_button_event.cpp
@@ -1,6 +1,6 @@
 #include <blackboard/editor/mouse_button_event.hpp>
 
-#include <raylib.h>
+#include <raymath.h>
 
 
 
@@ -11,7 +11,61 @@ void MouseButtonEvent::update() noexcept
     on_release.update();
     on_down.update();
 
-    if (IsMouseButtonPressed(button_id)) on_press.trigger();
-    if (IsMouseButtonReleased(button_id)) on_release.trigger();
-    if (IsMouseButtonDown(button_id)) on_down.trigger();
+    if (IsMouseButtonPressed(button_id)) trigger_press_event();
+    if (IsMouseButtonReleased(button_id)) trigger_release_event();
+    if (IsMouseButtonDown(button_id)) trigger_down_event();
+
+    update_drag_state();
+}
+
+
+void MouseButtonEvent::update_drag_state() noexcept
+{
+    if (!press_position_)
+        return;
+
+    if (!is_drag_ && distance_from_press_position() >= min_drag_distance_)
+    {
+        is_drag_ = true;
+        on_drag_start.trigger();
+    }
+}
+
+
+void MouseButtonEvent::trigger_press_event() noexcept
+{
+    press_position_ = mouse_position_provider.mouse_position();
+
+    on_press.trigger();
+}
+
+
+void MouseButtonEvent::trigger_release_event() noexcept
+{
+    on_release.trigger();
+
+    if (is_drag_)
+        on_drag_end.trigger();
+    else
+        on_click.trigger();
+
+    is_drag_ = false;
+    press_position_.reset();
+}
+
+
+void MouseButtonEvent::trigger_down_event() noexcept
+{
+    on_down.trigger();
+}
+
+
+
+
+float MouseButtonEvent::distance_from_press_position() const noexcept
+{
+    if (!press_position_)
+        return 0;
+
+    return Vector2Distance(*press_position_, mouse_position_provider.mouse_position());
 }
