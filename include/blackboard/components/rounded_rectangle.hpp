c64f3991f4bfc73adbdfcb9609c8254a3aedc7aa
Marvin Marjan <marvin.marjan@gmail.com>
2026-05-11 15:30:29 -0400

    add utility methods for converting radius/normalized radius in `RoundedRectangle`

--- include/blackboard/components/rounded_rectangle.hpp
+++ include/blackboard/components/rounded_rectangle.hpp
@@ -29,11 +29,13 @@ public:
     void set_size(const Vector2& size) noexcept { size_spring_.target = size; }
 
 
+    void set_normalized_radius(const float normalized_radius) noexcept { radius = get_radius_from_normalized(normalized_radius); }
+
+    float get_radius_from_normalized(const float normalized_radius) const noexcept { return normalized_radius * std::min(size_spring_.current.x, size_spring_.current.y); }
+    float get_normalized_radius(const float radius) const noexcept { return radius / std::min(size_spring_.current.x, size_spring_.current.y); }
+
+
 protected:
     void draw_filled() noexcept override;
     void draw_outlined() noexcept override;
-
-
-private:
-    float get_normalized_radius() const noexcept { return radius / std::min(size_spring_.current.x, size_spring_.current.y); }
 };
