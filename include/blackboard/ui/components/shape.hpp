932a02c418543a12e4bf0ab34dc34df7515823ab
Marvin Marjan <marvin.marjan@gmail.com>
2026-05-16 15:59:03 -0400

    add `Shape` color convenience methods

--- include/blackboard/ui/components/shape.hpp
+++ include/blackboard/ui/components/shape.hpp
@@ -19,13 +19,19 @@ public:
         const Color& outline_color = WHITE) noexcept;
 
 
-    const Color& color() const noexcept { return color_; }
+    const Color& color() const noexcept { return color_.current; }
     float outline_thickness() const noexcept { return outline_thickness_; }
-    const Color& outline_color() const noexcept { return outline_color_; }
+    const Color& outline_color() const noexcept { return outline_color_.current; }
+    uint8_t opacity() const noexcept { return color_.current.a; }
+    uint8_t outline_opacity() const noexcept { return outline_color_.current.a; }
 
     void set_color(const Color& color) noexcept { color_.target = color; }
     void set_outline_thickness(const float thickness) noexcept { outline_thickness_.target = thickness; }
     void set_outline_color(const Color& outline_color) noexcept { outline_color_.target = outline_color; }
+    void set_opacity(const uint8_t opacity) noexcept { color_.target.a = opacity; }
+    void set_outline_opacity(const uint8_t opacity) noexcept { outline_color_.target.a = opacity; }
+    void set_both_color(const Color& color) noexcept { color_.target = outline_color_.target = color; }
+    void set_both_opacity(const uint8_t opacity) noexcept { color_.target.a = outline_color_.target.a = opacity; }
 
 
 protected:
