74ee4bc4b441be23412b30fe8a0442f90ce7590d
Marvin Marjan <marvin.marjan@gmail.com>
2026-05-16 15:59:55 -0400

    rename `RadialMenu` to `RadialLayout`

--- include/blackboard/editor/ui/radial_layout.hpp
+++ include/blackboard/editor/ui/radial_layout.hpp
@@ -5,14 +5,14 @@
 
 
 
-class RadialMenu : public Component
+class RadialLayout : public Component
 {
-private:
+protected:
     Interpolation<float> radius_;
 
 
 public:
-    RadialMenu(Component* const parent, const Vector2& position, const float radius) noexcept;
+    RadialLayout(Component* const parent, const Vector2& position, const float radius) noexcept;
 
 
     Rectangle relative_bounding_box() const noexcept override {
@@ -20,9 +20,17 @@ public:
     }
 
 
+    float radius() const noexcept { return radius_; }
+
+    void set_radius(const float radius) noexcept { radius_.target = radius; }
+
+
 protected:
     void update_self() noexcept override;
     void update_children_position() noexcept;
 
     void draw_self() noexcept override {}
+
+
+    virtual Vector2 get_position_for_child(Component& child, const size_t i) noexcept;
 };
---
+++ src/editor/ui/radial_layout.cpp
@@ -0,0 +1,40 @@
+#include <blackboard/editor/ui/radial_layout.hpp>
+
+
+
+
+RadialLayout::RadialLayout(Component* const parent, const Vector2& relative_position, const float radius) noexcept
+    : Component(parent, relative_position), radius_(create_default_interpolation(radius))
+{}
+
+
+
+
+void RadialLayout::update_self() noexcept
+{
+    Component::update_self();
+
+    radius_.update();
+    update_children_position();
+}
+
+
+void RadialLayout::update_children_position() noexcept
+{
+    for (size_t i = 0; i < children.size(); i++)
+        children[i]->set_relative_position(get_position_for_child(*children[i], i));
+}
+
+
+
+
+Vector2 RadialLayout::get_position_for_child(Component& child, const size_t i) noexcept
+{
+    constexpr float CircunferenceLength = 2 * PI;
+    const float regular_length = CircunferenceLength / children.size();
+
+    const float angle = i * regular_length;
+    const Vector2 position = { cos(angle) * radius_, sin(angle) * radius_ };
+
+    return position;
+}
