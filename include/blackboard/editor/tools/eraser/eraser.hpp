06f5756de05b4c295682bb4ffe76a880e26513c9
Marvin Marjan <marvin.marjan@gmail.com>
2026-06-19 13:48:15 -0400

    implement erasing logic for eraser tool

--- include/blackboard/editor/tools/eraser/eraser.hpp
+++ include/blackboard/editor/tools/eraser/eraser.hpp
@@ -14,9 +14,19 @@ namespace bb::editor
 
 class Eraser : public Tool
 {
+private:
+    Vector2 last_position_ = {};
+
+    std::vector<const StrokeMesh*> strokes_to_remove_;
+
+
 public:
     using Tool::Tool;
 
+
+    size_t iteration_amount = 8;
+    size_t iteration_amount_test_ = 1;
+    bool dynamic_iteration_amount = true;
 
     EraserBody body;
 
@@ -29,9 +39,25 @@ public:
 
 
     [[nodiscard]] Vector2 position() const noexcept override;
+
+
+private:
+    [[nodiscard]] float distance_delta() const noexcept { return Vector2Length(position() - last_position_); }
+
+    [[nodiscard]] size_t get_iteration_amount() const noexcept {
+        return dynamic_iteration_amount ? std::max((size_t)((float)iteration_amount * distance_delta() / 50), iteration_amount) : iteration_amount;
+    }
+
+    void update_strokes_to_remove() noexcept;
+    StrokeMesh* get_canvas_stroke_at_point(const Vector2& point) noexcept;
+
+    void add_stroke_to_remove_queue(StrokeMesh& stroke) noexcept;
+
+    void remove_strokes_from_remove_queue() noexcept;
+    void remove_stroke(const StrokeMesh& stroke) noexcept;
 };
 
-
+// TODO: make StrokeMesh an own class instead of an alias and implement StrokeMesh::set_color()
 
 
 }
--- src/editor/tools/eraser/eraser.cpp
+++ src/editor/tools/eraser/eraser.cpp
@@ -1,11 +1,13 @@
 #include <blackboard/editor/tools/eraser/eraser.hpp>
 
 #include <blackboard/editor/canvas.hpp>
+#include <blackboard/editor/stroke_mesh_collider.hpp>
 
 
 
 
-using bb::editor::Eraser;
+using bb::editor::Eraser,
+    bb::editor::StrokeMesh;
 
 
 
@@ -19,7 +21,69 @@ Eraser::Eraser(Canvas& canvas) noexcept : Tool(canvas),
 
 void Eraser::update() noexcept
 {
+    Tool::update();
+
+    if (active())
+        update_strokes_to_remove();
+
+    if (got_inactive_)
+        remove_strokes_from_remove_queue();
+
+    iteration_amount_test_ = get_iteration_amount();
+
     body.update();
+    last_position_ = position();
+}
+
+
+void Eraser::update_strokes_to_remove() noexcept
+{
+    const Vector2 position = this->position();
+    const auto final_iteration_amount = get_iteration_amount();
+
+    for (size_t i = 0; i < final_iteration_amount; i++)
+    {
+        const Vector2 point = Vector2Lerp(last_position_, position, (float)i / (float)iteration_amount);
+
+        if (StrokeMesh* const stroke = get_canvas_stroke_at_point(point))
+            add_stroke_to_remove_queue(*stroke);
+    }
+}
+
+
+StrokeMesh* Eraser::get_canvas_stroke_at_point(const Vector2& point) noexcept
+{
+    for (auto& stroke : canvas_.stroke_meshes)
+        if (StrokeMeshCollider::stroke_contains_point(*stroke, point)) // TODO: use segment-segment intersection instead of point collision
+            return stroke.get();
+
+    return nullptr;
+}
+
+
+void Eraser::add_stroke_to_remove_queue(StrokeMesh& stroke) noexcept
+{
+    for (auto& mesh_node : stroke)
+        mesh_node.color.a = 150;
+
+    strokes_to_remove_.push_back(&stroke);
+}
+
+
+void Eraser::remove_strokes_from_remove_queue() noexcept
+{
+    for (auto& stroke : strokes_to_remove_)
+        remove_stroke(*stroke);
+
+    strokes_to_remove_.clear();
+}
+
+
+void Eraser::remove_stroke(const StrokeMesh& stroke) noexcept
+{
+    std::erase_if(canvas_.stroke_meshes, [&](const std::unique_ptr<StrokeMesh>& stroke_mesh) {
+        return stroke_mesh.get() == &stroke;
+    });
 }
 
 
